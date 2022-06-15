ea730c4298bac39eb212247387194a873f9c1969
Mohammed Ahmed <mo.ahmed0987@gmail.com>
2022-06-15 19:08:30 +0100

    cleaned up __init__ file

--- spotify_site/__init__.py
+++ spotify_site/__init__.py
@@ -1,18 +1,7 @@
 import secret_keys
 from flask import Flask
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secret_keys.app_config_secret_key
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "blog.db")
-
-# db = SQLAlchemy(app)
 
 from spotify_site import routes
