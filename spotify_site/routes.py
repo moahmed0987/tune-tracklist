eb25a758726c3df021a2fa8360ec75783e6f66a4
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2023-05-26 14:49:53 +0100

    change date format

--- spotify_site/routes.py
+++ spotify_site/routes.py
@@ -146,7 +146,7 @@ def clearsession():
     return redirect(url_for("home"))
 
 @app.template_filter('datetimeformat')
-def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
+def datetimeformat(value, format='%Y-%m-%d %H:%M'):
     from datetime import datetime
     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%fZ')
     return value.strftime(format)
